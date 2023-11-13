1538bfbc67025014b56ff6748c9f1a4ca00d1851
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-13 00:57:05 -0500

    Agrega funcionalidad para cambiar la escala del lienzo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 const sketch = document.querySelector('.sketch');
+const scale = document.querySelector('#scale');
+
+scale.addEventListener('input', (e) => {
+  const target = e.target;
+  let num = target.value;
+  createSketch(num);
+});
 
 sketch.addEventListener('mouseover', (e) => { changeColor(e.target); });
-createSketch();
+createSketch(16);
 
-function createSketch() {
-  for (let i = 0; i < 16 * 16; i++) {
+function createSketch(num) {
+  const oldSquares = document.querySelectorAll('.square');
+  oldSquares.forEach(e => {
+    e.remove();
+  });
+  for (let i = 0; i < num * num; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.style.width = 100 / num + '%';
+    square.style.height = 100 / num + '%';
     sketch.appendChild(square);
   }
 }
